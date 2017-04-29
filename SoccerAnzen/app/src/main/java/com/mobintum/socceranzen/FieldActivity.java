0d74aa1d3707c329311b1baa05de5c5f1865ea9b
Ricardo Centeno <Ricardo@MacBook-Pro-Rick.local>
2017-04-28 20:39:38 -0500

    Pass ball complete

--- SoccerAnzen/app/src/main/java/com/mobintum/socceranzen/FieldActivity.java
+++ SoccerAnzen/app/src/main/java/com/mobintum/socceranzen/FieldActivity.java
@@ -1,20 +1,73 @@
 package com.mobintum.socceranzen;
 
 import android.annotation.SuppressLint;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 
-public class FieldActivity extends AppCompatActivity {
+public class FieldActivity extends AppCompatActivity implements PlayerFragment.CallbacksFragment {
+
+    private TextView txtScoreX, txtScoreY;
+    private FrameLayout frameKeeperX, frameKeeperY, framePlayer1x, framePlayer2x, framePlayer1y, framePlayer2y;
+    private FragmentManager dtRicardo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_field);
+        dtRicardo = getSupportFragmentManager();
+        txtScoreX = (TextView) findViewById(R.id.txtScoreX);
+        txtScoreY = (TextView) findViewById(R.id.txtScoreY);
+        frameKeeperX = (FrameLayout) findViewById(R.id.frameKeeperX);
+        frameKeeperY = (FrameLayout) findViewById(R.id.frameKeeperY);
+        framePlayer1x = (FrameLayout) findViewById(R.id.framePlayer1x);
+        framePlayer2x = (FrameLayout) findViewById(R.id.framePlayer2x);
+        framePlayer1y = (FrameLayout) findViewById(R.id.framePlayer1y);
+        framePlayer2y = (FrameLayout) findViewById(R.id.framePlayer2y);
+        loadPlayers();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        PlayerFragment playerFragment = (PlayerFragment) dtRicardo.findFragmentByTag("Gerardo");
+        playerFragment.showBall(true);
+
+    }
+
+    private void loadPlayers(){
+        PlayerFragment playerGerardo = PlayerFragment.newInstance("Gerardo",R.color.yellow_america);
+        PlayerFragment playerEdgar = PlayerFragment.newInstance("Edgar",R.color.yellow_america);
+        PlayerFragment playerArmando = PlayerFragment.newInstance("Armando",R.color.yellow_america);
+        PlayerFragment playerCesar = PlayerFragment.newInstance("Cesar",R.color.blue_pumas);
+        PlayerFragment playerJesus = PlayerFragment.newInstance("Jesus",R.color.blue_pumas);
+        PlayerFragment playerDaniel = PlayerFragment.newInstance("Daniel",R.color.blue_pumas);
+
+        FragmentTransaction ft = dtRicardo.beginTransaction();
+        ft.add(R.id.frameKeeperX,playerGerardo,"Gerardo");
+        ft.add(R.id.frameKeeperY,playerCesar,"Cesar");
+        ft.add(R.id.framePlayer1y,playerJesus,"Jesus");
+        ft.add(R.id.framePlayer2y,playerDaniel,"Daniel");
+        ft.add(R.id.framePlayer1x,playerEdgar,"Edgar");
+        ft.add(R.id.framePlayer2x,playerArmando,"Armando");
+        ft.commit();
+
+    }
+
+    @Override
+    public void pass(int position) {
+        PlayerFragment playerFragment = (PlayerFragment) dtRicardo.getFragments().get(position);
+        if (playerFragment!=null)
+            playerFragment.showBall(true);
+
+    }
 }
--- SoccerAnzen/app/src/main/java/com/mobintum/socceranzen/PlayerFragment.java
+++ SoccerAnzen/app/src/main/java/com/mobintum/socceranzen/PlayerFragment.java
@@ -1,29 +1,96 @@
 package com.mobintum.socceranzen;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class PlayerFragment extends Fragment {
+public class PlayerFragment extends Fragment implements View.OnClickListener {
+    public static final String TAG = "PlayerFragment";
 
+    private static final String ARG_PARAM_NAME = "paramName";
+    private static final String ARG_PARAM_COLOR = "paramColor";
+    private String name;
+    private int tshirtColor;
+
+    private TextView txtName;
+    private ImageView imgTShirt;
+    private ImageView imgBall;
+
+    private CallbacksFragment callbacks;
 
     public PlayerFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        callbacks = (CallbacksFragment) context;
+    }
+
+    public static PlayerFragment newInstance(String name, int tshirtColor ) {
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM_NAME,name);
+        args.putInt(ARG_PARAM_COLOR,tshirtColor);
+        PlayerFragment fragment = new PlayerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if( getArguments()!=null ){
+            this.name = getArguments().getString(ARG_PARAM_NAME);
+            this.tshirtColor = getArguments().getInt(ARG_PARAM_COLOR);
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_player, container, false);
+        View view = inflater.inflate(R.layout.fragment_player, container, false);
+        txtName = (TextView) view.findViewById(R.id.txtName);
+        imgTShirt = (ImageView) view.findViewById(R.id.imgTShirt);
+        imgBall = (ImageView) view.findViewById(R.id.imgBall);
+        imgBall.setOnClickListener(this);
+        txtName.setText(name);
+        imgTShirt.setColorFilter(getResources().getColor(tshirtColor));
+        return view;
     }
 
+    @Override
+    public void onClick(View view) {
+        showBall(false);
+        callbacks.pass(getRandomPosition(0,6));
+    }
+
+    public static int getRandomPosition(int a, int b){
+        Random r = new Random();
+        int result = r.nextInt(b-a) + a;
+        return result;
+    }
+
+    public void showBall(boolean show){
+        if (show)
+            imgBall.setVisibility(View.VISIBLE);
+        else
+            imgBall.setVisibility(View.GONE);
+    }
+
+    public interface CallbacksFragment{
+        public void pass(int position);
+    }
 }
